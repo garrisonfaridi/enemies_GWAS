a9e695d55c5f64413b4065a4bc6659bf8cb565f1
garrisonfaridi <gpf2024@nyu.edu>
2025-07-08 19:25:12 -0400

    mvlmm_allClusts.sh

--- enemy_GWAS_scripts/sbatch_GEMMA_HPC/mvlmm_allClusts.sh
+++ enemy_GWAS_scripts/sbatch_GEMMA_HPC/mvlmm_allClusts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=gemma_array
-#SBATCH --array=1-16          # Update if you have more clusters
+#SBATCH --array=1-16         # Update if you have more/less clusters
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=24G
 #SBATCH --output=logs/gemma_cluster_%A_%a.out
@@ -10,16 +10,17 @@
 module load gemma/0.98.4
 
 # Define paths
-PHENO_DIR="impk7_knn5_t100_pears_0.35"
-OUT_DIR="gemma_outputs_clusters_impk7_knn5_t100_pears_0.35"
-KINSHIP="output/kinship_enemy_305.cXX.txt"
+# Replace paths with your current run
+PHENO_DIR="impk7_pca_clusters_cut0.22" 
+OUT_DIR="gemma_outputs_clusters_kin0.1_pca_cut0.22"
+KINSHIP="output/kinship_enemy_305_impk7_miss0.1.cXX.txt"
 BED="genotypes/genotypes_305"
 
 # Create output directory if needed
 mkdir -p $OUT_DIR
 
 # Run GEMMA on the phenotype file for this cluster
-PHENO_FILE="${PHENO_DIR}/cluster_${SLURM_ARRAY_TASK_ID}_impk7_knn5_t100_pears_0.35.phenos.txt"
+PHENO_FILE="${PHENO_DIR}/cluster_${SLURM_ARRAY_TASK_ID}_impk7_pca_cut0.22.phenos.txt"
 
 # Run GEMMA for MVLMM
 gemma -lmm 1 \
@@ -28,6 +29,5 @@ gemma -lmm 1 \
       -bfile $BED \
       -k $KINSHIP \
       -p $PHENO_FILE \
-      -maf 0.03 \
       -o cluster_${SLURM_ARRAY_TASK_ID} \
       -outdir $OUT_DIR
